4f2c36616579299417425a8c69b07d796e49f287
Keating Reid <keating.reid@protonmail.com>
2020-11-10 10:30:01 -0500

    changing contact email

--- contact_onclick.js
+++ contact_onclick.js
@@ -5,5 +5,5 @@ function contact() {
             return String.fromCharCode(s.charCodeAt(0)+(s.toLowerCase()<'n'?13:-13))
         })
     }
-    window.open(str_rot13(atob("em52eWdiOnhybmd2YXQuZXJ2cUB6bnZ5LnpwdHZ5eS5wbg==")))
+    window.open(str_rot13(atob("oJScoUEiBzgyLKEcozphpzIcMROjpz90o25gLJyfYzAioD==")))
 }
