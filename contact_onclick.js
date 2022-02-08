38edb77fa4033f28227c20676dd291247b8a4159
Keating Reid <keating.reid@protonmail.com>
2022-02-08 09:10:39 -0500

    Removing email scrambling in favour of iCloud "Hide my Email" address

--- contact_onclick.js
+++
@@ -1,9 +0,0 @@
-function contact() {
-    "use strict";
-    function str_rot13(str){
-        return (str+'').replace(/[a-zA-Z]/gi, function(s) {
-            return String.fromCharCode(s.charCodeAt(0)+(s.toLowerCase()<'n'?13:-13))
-        })
-    }
-    window.open(str_rot13(atob("oJScoUEiBzgyLKEcozphpzIcMROjpz90o25gLJyfYzAioD==")))
-}
